4c07093549a01ce424929578ed6976c660969e29
dungdauten <nguyenanhdung1922@gmail.com>
2024-03-11 20:27:21 +0700

    Create Project

---
+++ FlightSystemManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FlightSystemManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
